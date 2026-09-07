2d01f2311cc8642f001d039d3107346e81767c09
Johan Yadir Huayta Enriquez <johan.huayta@utec.edu.pe>
2026-09-07 15:44:50 -0500

    Modify test cases and expected results in test.cpp
    Updated test data and output expectations in test.cpp.

--- test.cpp
+++ test.cpp
@@ -7,17 +7,17 @@ int main() {
     std::cout << "--- PRUEBAS CORE_NUMERIC ---" << std::endl;
 
     // 1. CASOS QUE COMPILAN CORRECTAMENTE
-    std::vector<double> datos = {2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0};
+    std::vector<double> datos = {4.0, 24.0, 10.0, 0.0, 9.0, 67.0, 67.0, 19.0, 1.0, 19.0, 10.0, 5.0, 25.0};
 
     auto total = core_numeric::sum(datos);
     auto promedio = core_numeric::mean(datos);
     auto varianza = core_numeric::variance(datos);
+ 
+    std::cout << "Suma: " << total << std::endl;   // El resultado deberia ser 260
+    std::cout << "Promedio: " << promedio << std::endl; // El promedio deberia ser 20
+    std::cout << "Varianza: " << varianza << std::endl;  // La varianza deberia ser 463,3846 aproximadamente
 
-    std::cout << "Suma: " << total << std::endl;         // Esperado: 40
-    std::cout << "Promedio: " << promedio << std::endl;   // Esperado: 5
-    std::cout << "Varianza: " << varianza << std::endl;   // Esperado: 4
-
-    // 2. CASOS QUE NO COMPILAN POR CONCEPTS (Se dejan comentados según la guía)
+    // 2. CASOS QUE NO COMPILAN
     /*
     std::vector<std::string> palabras = {"Hola", "Mundo"};
     
